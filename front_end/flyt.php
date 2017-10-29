d4fb323d724c2bcb9cb5082f3da1e03d650d58b7
kzhang21 <kellyzhang2197@gmail.com>
2017-10-29 11:51:56 -0400

    get json from http and turn str into timestamp

--- front_end/flyt.php
+++ front_end/flyt.php
@@ -41,9 +41,33 @@
     $myObj->airport = $_GET["air"];
     $myObj->flightId = $_GET["flyid"];
     $myObj->selfCheck = $_GET["self"];
-    $myObj->departTime = str_replace("T", " ", $_GET["departure"]);
+    $myObj->departTime = strtotime(str_replace("T", " ", $_GET["departure"]));
 
-    $myJSON = json_encode($myObj);
+    $content = json_encode($myObj);
+
+    $url = "http://127.0.0.1:5000/";    
+    
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER,
+            array("Content-type: application/json"));
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+    $json_response = curl_exec($curl);
+
+    $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+    if ( $status != 201 ) {
+        die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+    }
+
+
+    curl_close($curl);
+
+    $response = json_decode($json_response, true);
 
     //echo $myJSON;
     ?>
